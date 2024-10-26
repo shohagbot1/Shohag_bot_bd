0c3483a851fd1e8d620a6e0ee77b397ed358c869
LisNis <cyberlisa92@gmail.com>
2024-10-26 12:02:54 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "😠"){
-  if(event.userID == "100080682514874"){
+  if(event.userID == "61567840496026"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😾"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100080682514874"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61567840496026"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
